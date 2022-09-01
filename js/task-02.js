330f9e01f139f1b8c25d4117d5427d9c7457b2fb
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-01 19:17:23 +0300

    add task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, "");
+
+ulEl.innerHTML = list;
