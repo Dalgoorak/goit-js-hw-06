7ea0e69dec342941b103f711879ffcf5d112f7fb
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-04 17:56:08 +0300

    add task 5

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("#name-input");
+const nameLabel = document.querySelector("#name-output");
+
+input.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
+  if (event.currentTarget.value === "") {
+    nameLabel.textContent = "Anonymous";
+  } else {
+    nameLabel.textContent = event.currentTarget.value;
+  }
+}
