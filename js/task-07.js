901c6f8e36f9c69bc9caa4baabee259200de10da
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-06 15:21:14 +0300

    Update

--- js/task-07.js
+++ js/task-07.js
@@ -5,3 +5,9 @@ const sizeControle = document.querySelector("#font-size-control");
 sizeControle.addEventListener("input", () => {
   textEl.style.fontSize = sizeControle.value + "px";
 });
+
+// second variable===========
+
+// sizeControle.addEventListener("input", (event) => {
+//   text.style.fontSize = `${event.currentTarget.value}px`;
+// });
