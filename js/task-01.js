e260b1e967d7300007c67bb7588c09280f01f217
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-06 14:31:03 +0300

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,12 @@
-const categoriesList = document.querySelectorAll(".item");
+const categoriesList = document.querySelector("#categories").children;
 console.log(`Number of categories: ${categoriesList.length}`);
 
 categoriesList.forEach((item) => {
   console.log(`Category: ${item.querySelector("h2").textContent}`);
   console.log(`Elements: ${item.querySelectorAll("li").length}`);
 });
+
+// categoriesList.forEach((item) => {
+//   console.log(`Category: ${item.firstElementChild.textContent}`);
+//   console.log(`Elements: ${item.lastElementChild.children.length}`);
+// });
