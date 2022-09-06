636403e636477800db9b0d43d1c89c27c3cfdbd2
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-06 12:46:08 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const categoriesList = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoriesList.length}`);
 
 categoriesList.forEach((item) => {
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  console.log(`Elements: ${item.lastElementChild.children.length}`);
+  console.log(`Category: ${item.querySelector("h2").textContent}`);
+  console.log(`Elements: ${item.querySelectorAll("li").length}`);
 });
